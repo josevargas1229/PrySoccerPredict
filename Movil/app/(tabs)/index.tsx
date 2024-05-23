9cda39946f0bfa7dbc8fb0812677f5aedf9526b5
josevargas1229 <20221015@uthh.edu.mx>
2024-05-22 20:48:42 -0600

    Base para mostrar los partidos y animacion

--- Movil/app/(tabs)/index.tsx
+++ Movil/app/(tabs)/index.tsx
@@ -1,56 +1,40 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import FootballMatchesList from '@/components/FootballMatchesList';
+import RotatingBall from '@/components/RotatingBall';
 
 export default function HomeScreen() {
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: '#0CA80D', dark: '#195E19' }}
       headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+        <View style={styles.headerContainer}>
+          <View style={styles.rotatingBallContainer}>
+            <RotatingBall />
+          </View>
+        </View>
+      }
+      >
+      <FootballMatchesList/>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  headerContainer: {
+    height: '100%', // Ajusta según el tamaño del header que necesites
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    position: 'relative', // Asegúrate de que el contenedor tenga posición relativa
+  },
+  rotatingBallContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    padding: 10, // Ajusta el padding según sea necesario
+  },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
